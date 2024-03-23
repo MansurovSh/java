9ad2b0871248d58639d0a44cecc4b7f36e19ce3a
MansurovSh <147724618+MansurovSh@users.noreply.github.com>
2024-03-23 22:48:27 +0500

    Added Lesson_4

---
+++ Lesson_4/DataContainerControll.java
@@ -0,0 +1,38 @@
+package Lesson_4;
+
+public class DataContainerControll<T> {
+    private DataContainerModel<T> model;
+    private DataContainerView view;
+
+    public DataContainerControll(DataContainerModel<T> model, DataContainerView view){
+        this.model = model;
+        this.view = view;
+    }
+
+    public void getUserInput(){
+        boolean running = true;
+        while (running) {
+            int choise = view.getUserChoise();
+
+            switch (choise) {
+                case 1:
+                    //model.Add(view.getT());
+                    break;
+                case 2:
+                    System.out.println(model.get(view.getindex()));
+                    break;
+                case 3:
+                    model.remove(view.getindex());
+                    break;
+                case 4:
+                    System.out.println(model.getSize());
+                    break;
+                case 5:
+                    running = false;
+                    break;
+                default:
+                    System.out.println("Неверная команда");
+            }
+        }
+    }
+}
---
+++ Lesson_4/Main.java
@@ -0,0 +1,12 @@
+package Lesson_4;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        DataContainerModel<String> model = new DataContainerModel<>();
+        DataContainerView view = new DataContainerView();
+        DataContainerControll<String> controll = new DataContainerControll<>(model, view);
+        
+        controll.getUserInput();
+    }
+}
